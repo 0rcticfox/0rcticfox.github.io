06babad77b8722f6613e0752fe72eb2ef146aabc
0rctic_fox <0rctic_fox@naver.com>
2023-01-12 23:37:30 +0900

    32차 수정 - 원복 및 속도 줄임

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,13 +10,13 @@
   // Hero typed
   if ($(".typed").length) {
     var typed_strings = $(".typed").data("typed-items");
-    typed_strings = typed_strings.split("/");
+    typed_strings = typed_strings.split(",");
     new Typed(".typed", {
       strings: typed_strings,
       loop: true,
       typeSpeed: 100,
       backSpeed: 50,
-      backDelay: 1000,
+      backDelay: 700,
     });
   }
 
